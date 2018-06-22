92634d668f35711942510da2bf7a7d347258e65b
vison.cao <vison.cao@huolala.cn>
2018-06-22 20:32:40 +0800

    修改安装脚本

--- setup.py
+++ setup.py
@@ -8,6 +8,9 @@ setup(
     author='visonforcoding',
     author_email='visonforcoding@gmail.com',
     url='https://github.com/visonforcoding/myssh',
+    install_requires=[
+        'click>=6.7',
+    ],
     package_data={
         "": ["dataset/server.db"],
     },
