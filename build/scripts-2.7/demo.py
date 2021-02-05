14c432c51de1e3ca88179c5a8601eccaf95e8c8d
vison.cao <vison.cao@huolala.cn>
2021-02-05 09:34:02 +0800

    delete info

--- build/scripts-2.7/demo.py
+++ build/scripts-2.7/demo.py
@@ -2,10 +2,8 @@
 import click
 import subprocess
 
-#subprocess.call(['ssh', '-p', '62130', 'vison@dev.huolala.cn'])
 
 config = {
-    1: {'port': '62130', 'host': 'vison@dev.huolala.cn'}
 }
 
 
@@ -13,7 +11,6 @@ config = {
 def main():
     click.echo(click.style('welcome login ssh server by vison .v1.0', fg='red'))
     click.echo(click.style('可选的服务列表:', fg='red'))
-    click.echo(click.style('   1) dev.huolala.cn', fg='white'))
     value = click.prompt('输入要登录的host', type=int)
     if value in config:
         subprocess.call(['ssh','-p',config[value]['port'],config[value]['host']])
